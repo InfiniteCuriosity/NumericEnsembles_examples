b22e4a7014a6c4858683c6e178b585cad35642b5
Russ Conte <russconte@mac.com>
2026-02-27 09:52:06 -0600

    Analysis of BMW sales and price data

---
+++ BMW.R
@@ -0,0 +1,29 @@
+#install.packages(NumericEnsembles)
+
+start_time <- Sys.time()
+
+library(NumericEnsembles)
+Numeric(data = read.csv('https://raw.githubusercontent.com/InfiniteCuriosity/EnsemblesData/refs/heads/main/bmw.csv', stringsAsFactors = TRUE),
+  colnum = 3,
+  numresamples = 25,
+  remove_VIF_above = 5.00,
+  remove_data_correlations_greater_than = 0.99,
+  remove_ensemble_correlations_greater_than = 1.00,
+  scale_all_predictors_in_data = "N",
+  data_reduction_method = 0,
+  ensemble_reduction_method = 0,
+  how_to_handle_strings = 1,
+  predict_on_new_data = "N",
+  save_all_trained_models = "N",
+  stratified_random_column = 4,
+  set_seed = "N",
+  save_all_plots = "Y",
+  use_parallel = "Y",
+  train_amount = 0.60,
+  test_amount = 0.20,
+  validation_amount = 0.20)
+  
+  end_time <- Sys.time()
+  duration <- end_time - start_time
+  duration
+  warnings()
